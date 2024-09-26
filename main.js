1ee7eed32c9414b08afd095903c1c6db17db7cd2
Developer-Kings-Dev <developerkingsdevops@gmail.com>
2024-09-25 12:02:39 -1200

    Hidden Secrets

--- main.js
+++ main.js
@@ -7,11 +7,18 @@ const app = express();
 const port = process.env.PORT || 8000;
 const serviceAccount = require("./firebaseadminsdk.json");
 
+// Load Firebase credentials from environment variables
+const serviceAccount = {
+  "type": "service_account",
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+};
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   storageBucket: "gs://chasing-horizons-b6560.appspot.com"
 });
-
 
 
 connectDB();
